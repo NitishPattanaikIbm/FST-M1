de05633c38328773bb5c118c16e16d0f75a92612
NitishPattanaikIbm <nitish.pattanaik@ibm.com>
2024-09-20 20:35:49 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity3.java
@@ -0,0 +1,5 @@
+package com.example.fst_m1_testng;
+
+public class Activity3 {
+
+}
---
+++ TestNG/Activities/DemoTwo.java
@@ -0,0 +1,10 @@
+package com.example.fst_m1_testng;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+	 @Test
+	    public void TestCase() {
+	        System.out.println("I'm in the test case from DemoTwo Class");
+	    }
+	}
